00bbaa8cfa45786b0cc88c1ce189565eb2a4f8e9
Vladimir Grinenko <i@tadatuta.com>
2015-06-24 00:34:03 +0300

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+enb-html-to-bemjson
+===================
+
+Лицензия
+--------
+
+© 2014 YANDEX LLC. Код лицензирован [Mozilla Public License 2.0](LICENSE.txt).
---
+++ techs/html-to-bemjson.js
@@ -0,0 +1,39 @@
+/**
+ * html-to-bemjson
+ * =================
+ *
+ * Собирает *bemjson*-файл на основе *html*.
+ *
+ * **Опции**
+ *
+ * * *String* **source** — Исходный html-файл. По умолчанию — `?.html`.
+ * * *String* **target** — Результирующий BEMJSON-файл. По умолчанию — `?.bemjson.js`.
+ *
+ * **Пример**
+ *
+ * ```javascript
+ * nodeConfig.addTech(require('enb-html-to-bemjson/techs/html-to-bemjson'));
+ * ```
+ */
+var vfs = require('enb/lib/fs/async-fs'),
+    html2bemjson = require('html2bemjson');
+
+module.exports = require('enb/lib/build-flow').create()
+    .name('enb-html-to-bemjson')
+    .target('target', '?.bemjson.js')
+
+    .useSourceFilename('htmlFile', '?.html')
+    .optionAlias('htmlFile', 'htmlFileTarget')
+
+    .optionAlias('target', 'destTarget')
+    .builder(function(htmlFileName) {
+
+        return vfs.read(htmlFileName, 'utf-8')
+            .then(function(html) {
+                return '(' + require('util').inspect(html2bemjson.convert(html), { depth: null }) + ')';
+            })
+            .fail(function(data) {
+                console.log('Fail with: ', data);
+            });
+    })
+    .createTech();
